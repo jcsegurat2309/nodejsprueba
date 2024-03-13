2a26eaae996a77be2c86d1a04db088cfeb9b5555
jcsegurat2309 <jcsegurat@gmail.com>
2024-03-12 21:25:41 -0600

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
-
 const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
